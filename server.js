171ccca5c0a52374706a514bb4bc34ec561dd0cf
Aumer6 <aumerhayat420@gmail.com>
2026-05-18 11:04:38 +0430

    fix render port issue

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ app.get("/", (req, res) => {
   res.send("Atel Store Server is Running 🚀");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
